b8fdcfb5a5a40896ba3faf12e2b665c3e5911766
_rap7or <32204846+rap7or22@users.noreply.github.com>
2020-04-30 02:05:14 -0400

    REEE

--- halo/entrypoint.sh
+++ halo/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-function cleanup() {
-    # Delete all the old interfaces
-    echo "Deleting interfaces"
-    python3 cleanup.py
-}
-
-#trap 'cleanup' SIGTERM
-
 "${@}" &
 python3 haloGenerate.py
 exec nginx -g "daemon off;" &
